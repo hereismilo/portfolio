1ca3b53defaa4a250f5edc564fc1bd11201a0d8d
milo <hereismilo@gmail.com>
2026-03-29 21:58:07 +0100

    finished!

--- portfolio/obj/Debug/net10.0/portfolio.AssemblyInfo.cs
+++ portfolio/obj/Debug/net10.0/portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbe31762f573db59a109600c0b184e94257b3ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5816bbfd73fbe7d6b9d1efbb9d855ce12c481e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
